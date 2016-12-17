9a81f2a6779d6835b4840f64189da1e78f5fa2da
noodleutopia <noodleutopia@outlook.com>
2016-12-17 21:26:06 +0800

    【配置更改】禁止横屏，去掉actionbar

--- app/src/main/java/ss/pku/vibrantdiary/MainActivity.java
+++ app/src/main/java/ss/pku/vibrantdiary/MainActivity.java
@@ -2,12 +2,16 @@ package ss.pku.vibrantdiary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
